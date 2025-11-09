6faeb409339a1816e4375d40a4c49000e4a22b22
EnochBoss <enoch.bosomtwe@gmail.com>
2025-11-09 23:43:15 +0000

    Add Typescript project task 2

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -84,3 +84,22 @@ console.log(createEmployee('$500')); // Director
 console.log(createEmployee(200).constructor.name); // Teacher
 console.log(createEmployee(1000).constructor.name); // Director
 console.log(createEmployee('$500').constructor.name); // Director
+
+
+// Type predicate function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Execute work function
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Examples
+console.log(executeWork(createEmployee(200)));  // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
